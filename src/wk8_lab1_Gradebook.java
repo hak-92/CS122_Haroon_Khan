75afe3344ce1789424498efefe114d0902e324c5
s0mebody <haroon.a.kh@gmail.com>
2013-10-15 19:34:11 -0700

    commit

---
+++ src/wk8_lab1_Gradebook.java
@@ -0,0 +1,8 @@
+
+public class wk8_lab1_Gradebook {
+
+	public void displayMessage()
+	{	
+		System.out.println("Welecome to grade book!");
+	}
+}
---
+++ src/wk8_lab1_GradebookTest.java
@@ -0,0 +1,10 @@
+
+public class wk8_lab1_GradebookTest {
+
+	public static void main( String [] Args)
+	{
+		wk8_lab1_Gradebook myGradebook = new wk8_lab1_Gradebook ();
+		
+		myGradebook.displayMessage();
+	}
+}
