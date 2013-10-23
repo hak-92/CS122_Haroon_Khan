b9f3f6987fc5b141a4dfc0f6c2fa35528916bc6e
s0mebody <haroon.a.kh@gmail.com>
2013-10-22 19:45:37 -0700

    commit

---
+++ src/wk8_lab2_GuessANumber.java
@@ -0,0 +1,11 @@
+
+public class wk8_lab2_GuessANumber {
+	
+	public static void main (String [] args)
+	{
+		wk8_lab2_RandomNum RandomNum = new wk8_lab2_RandomNum();
+		
+		
+	}
+
+}
---
+++ src/wk8_lab2_RandomNum.java
@@ -0,0 +1,9 @@
+
+public class wk8_lab2_RandomNum {
+
+	public void GetANumber_Between_1_and_10()
+	{
+		
+		int computerNum = 0 + (int)(Math.random()*10);
+	}
+}
