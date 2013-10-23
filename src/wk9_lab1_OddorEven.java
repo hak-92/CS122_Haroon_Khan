d6a135ee566c2d440f717b2d0bb4f14b845ce85b
s0mebody <haroon.a.kh@gmail.com>
2013-10-22 19:44:20 -0700

    commit

---
+++ src/wk9_lab1_OddorEven.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+
+public class wk9_lab1_OddorEven {
+
+		public static void odd()
+		{
+			JOptionPane.showMessageDialog(null, "Odd");
+		}
+
+		
+		public static void even()
+		{
+			JOptionPane.showMessageDialog(null, "Even");
+		}
+}
---
+++ src/wk9_lab1_testobject.java
@@ -0,0 +1,12 @@
+
+public class wk9_lab1_testobject {
+	
+	public static void main (String [] args)
+	{
+		wk9_lab1_OddorEven test = new wk9_lab1_OddorEven();
+		
+		test.odd();
+		test.even();
+	}
+
+}
