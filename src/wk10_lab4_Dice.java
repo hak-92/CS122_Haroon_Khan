a43f6e9d8be010b80d620411f40762a33c29fd73
s0mebody <haroon.a.kh@gmail.com>
2013-10-29 20:51:53 -0700

    commit

---
+++ src/wk10_lab4_Dice.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+
+public class wk10_lab4_Dice {
+	
+	
+	 static int dnum;
+	 
+		
+	public static void OneThrow()
+	{
+	
+		dnum = (int)( Math.random()*(6-1)) + 1;
+		JOptionPane.showMessageDialog(null,"The number is " + dnum);
+		
+	}
+}
---
+++ src/wk10_lab4_test.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+
+public class wk10_lab4_test {
+	public static void main(String[] args) {
+		
+		wk10_lab4_Dice test = new wk10_lab4_Dice();
+		 
+		test.OneThrow();
+		
+	
+	
+	
+	}
+
+}
